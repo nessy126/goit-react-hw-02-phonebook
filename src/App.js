d32b7a5cd2d617d7424cca2e3b60dcaf40963ff9
nessy126 <nessy126@gmail.com>
2022-02-04 21:14:04 +0200

    second day when I try to do it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react/cjs/react.production.min"
+import { nanoid } from "nanoid"
+import "./App.css"
+import Form from "./components/Form/Form"
+import ContactList from "./components/ContactList/ContactList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+    number: "",
+  }
+
+  handleInputChange = (e) => {
+    console.log(e.target.name + ' :' + e.target.value)
+    // this.setState([e.target.name]: e.target.value)
+    const { name } = e.target.name
+    const { number } = e.target.number
+
+    this.setState({ name: name, number: number })
+      console.log(this.state)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(e.target.name.name) //tag input name - значение атрибута name
+    // console.log(e.target.number.name) //tag input number - значение атрибута name
+
+    // console.log(e.target.name.value)
+    // console.log(e.target.number.value)
+    const newContact = {
+      name: this.state.name,
+      id: nanoid(),
+    }
+    this.setState({
+      name: e.target.name.value,
+      number: e.target.number.value,
+    })
+
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <Form
+          handleInputChange={this.handleInputChange}
+          contacts={this.state.contacts}
+          name={this.state.name}
+          handleSubmit={this.handleSubmit}
+        />
+        <ContactList />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+const ContactList = ({contacts}) => {
+  return ( 
+    <>
+      <h2>Contacts</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+   );
+}
+ 
+export default ContactList;
